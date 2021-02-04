ef2eeaa3db37f8e18d9404a5f9f6eaf4499d6123
ElianaGomez2020 <elianagomez.s@outlook.com>
2021-02-04 12:17:38 -0500

    How many subs

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""function that queries the Reddit API and
+    returns the number of subscribers"""
+
+import requests
+
+def number_of_subscribers(subreddit):
+    """ Reddit API"""
+    respons = requests.get("https://www.reddit.com/r/{}/about.json"
+                           .format(subreddit), headers={'User-agent': 'hello'})
+    
+    if not respons:
+        return 0
+    return(respons.json().get('data').get('subscribers'))
