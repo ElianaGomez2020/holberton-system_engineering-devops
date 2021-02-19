ed66af1498901e44b842ab11bc9ebdcb05b56a7a
ElianaGomez2020 <elianagomez.s@outlook.com>
2021-02-18 19:00:44 -0500

    Web stack debugg #3

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
-# Fix file .phpp to .php 
-exec {'fix-file':
-    command => 'sed -i "s/phpp/php/" /var/www/html/wp-settings.php',
+# Fix file .phpp to .php
+exec { 'fix-file':
+    command => 'sudo sed -i "s/phpp/php/" /var/www/html/wp-settings.php',
     path    => ['/usr/bin', '/usr/sbin', '/usr/local/bin', '/usr/local/sbin'],
 }
